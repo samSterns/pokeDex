80c2d244b7f21f222a6a9ecd58890677a9867d6d
samSterns <sam.g.sterns@gmail.com>
2019-11-01 18:08:40 -0700

    header renders

--- Header.js
+++ Header.js
@@ -1,16 +1,18 @@
 import Component from './Component.js';
 
 class Header extends Component {
-    renderHtml(){
+    renderHTML(){
         return /*html*/ `
-        <header class="header">
-        <nav class="main-nav">
-            <div class="nav-home"><a href="">Home</div>
-            <div class="nav-home"><a href="">PlaceHolder</div>
-        </nav>
+        <section>
+            <header class="header">
+                <nav class="main-nav">
+                    <div class="nav-home"><a href="">Home</a></div>
+                    <div class="nav-home"><a href="">PlaceHolder</a></div>
+                </nav>
 
-        <h1>PokeDex</h1>
-    </header>
+                <h1>PokeDex</h1>
+            </header>
+        </section>
         `;
     }
 
--- pokedex/pokedexApp.js
+++ pokedex/pokedexApp.js
@@ -2,7 +2,7 @@ import Component from '../Component.js';
 import Header from '../Header.js';
 import SearchOptions from './SearchOptions.js';
 import Paging from './Paging.js';
-import PokeList from '../pokedex/PokeList.js';
+import PokeList from './PokeList.js';
 import { getPoke } from '../services/pokedex-api.js';
 
 class PokedexApp extends Component {
@@ -10,29 +10,32 @@ class PokedexApp extends Component {
     async onRender(dom){
         
         const header = new Header();
+        console.log(header.renderDOM());
+        
         dom.prepend(header.renderDOM());
 
-        const optionsSection = dom.querySelector('.searchbar-section');
-        const searchOptions = new SearchOptions();
-        optionsSection.prepend(searchOptions.renderDOM());
+        // const optionsSection = dom.querySelector('.searchbar-section');
+        // const searchOptions = new SearchOptions();
+        // optionsSection.prepend(searchOptions.renderDOM());
 
-        const listSection = dom.querySelector('.paging-div');
-        const paging = new Paging();
-        listSection.appendChild(paging.renderDOM());
+        // const listSection = dom.querySelector('.paging-div');
+        // const paging = new Paging();
+        // listSection.appendChild(paging.renderDOM());
 
-        const pokeList = new PokeList({ movies: [] });
-        listSection.appendChild(pokeList.renderDOM());
+        // const pokeList = new PokeList({ movies: [] });
+        // listSection.appendChild(pokeList.renderDOM());
 
-        const response = await getPoke();
-        const pokemon = response.Search;
-        pokeList.update({ pokemon: pokemon });
+        // const response = await getPoke();
+        // const pokemon = response.Search;
+        // pokeList.update({ pokemon: pokemon });
 
     }
 
     renderHTML() {
         return /*html*/ `
         <div>
-
+            <main>
+            </main>
         </div>
         `;
     }
