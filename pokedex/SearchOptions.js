44094f21ed59dbd1d9c387875ff1b9ad794dfc5d
samSterns <sam.g.sterns@gmail.com>
2019-11-01 18:08:26 -0700

    finished imports found

---
+++ pokedex/SearchOptions.js
@@ -0,0 +1,19 @@
+  
+import Component from '../Component.js';
+
+class SearchOptions extends Component {
+
+    renderHTML() {
+        return /*html*/ `
+            <div class="searchbar-section">
+                <form class="search">
+                    <div>Search:</div>
+                    <input name="search">
+                    <button>🔍</button>
+                </form>
+            </div>
+        `;
+    }
+}
+
+export default SearchOptions;
